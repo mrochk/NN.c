5aa3250264f5c2864c38b2e688b42e66bc305098
Maxime Rochkoulets <maxime.rochkoulets2@mail.dcu.ie>
2024-08-23 13:49:30 +0200

    some more comments

--- examples/nn/example.c
+++ examples/nn/example.c
@@ -12,6 +12,8 @@
 #define SAMPLES 100
 #define FEATURES 3
 
+/* since MSE computes the loss of the predictions given as a vector
+   we need to convert the output predictions to a vector */
 void forward(NN nn, Matrix X, Matrix temp, Vector preds) {
     nn_forward_batch(nn, X, temp); 
     vector_copy_matrix(preds, temp);
@@ -35,18 +37,16 @@ void compute_grads_(NN nn, Matrix X, Matrix temp, Vector preds, Vector y,
             b->d[i] += eps;
 
             forward(nn, X, temp, preds);
-
             float temploss_plus = MSE(preds, y);
 
-            b->d[i] -= eps;
-            b->d[i] -= eps;
+            b->d[i] -= 2.F * eps;
 
             forward(nn, X, temp, preds);
-
             float temploss_minus = MSE(preds, y);
 
             b->d[i] += eps;
 
+            /* symmetric derivative formula approx */
             db->d[i] = (temploss_plus - temploss_minus) / (2.f*eps);
         }
 
@@ -55,25 +55,22 @@ void compute_grads_(NN nn, Matrix X, Matrix temp, Vector preds, Vector y,
                 dw->d[i]->d[j] += eps;
 
                 forward(nn, X, temp, preds);
-
                 float temploss_plus = MSE(preds, y);
 
-                dw->d[i]->d[j] -= eps;
-                dw->d[i]->d[j] -= eps;
+                dw->d[i]->d[j] -= 2.F * eps;
 
                 forward(nn, X, temp, preds);
-
                 float temploss_minus = MSE(preds, y);
 
                 dw->d[i]->d[j] += eps;
 
+                /* symmetric derivative formula approx */
                 dw->d[i]->d[j] = (temploss_plus - temploss_minus) / (2.f*eps);
             }
         }
 
         dws[l] = dw;
         dbs[l] = db;
-
     }
     
     return;
@@ -93,13 +90,13 @@ void neuralnet_run_eg(int iters) {
     }
 
     puts("X:"); matrix_print(X);
-
     puts("y:"); vector_print(y);
 
     /*** init model ***/
 
     const uint nlayers = 4;
 
+    /* the architecture of the network */
     UintPair arch[] = { 
         pair(3,  10), 
         pair(10, 10), 
@@ -111,11 +108,8 @@ void neuralnet_run_eg(int iters) {
 
     /*** initial preds ***/
 
-    /* since MSE computes the loss of the predictions given as a vector
-       we need to convert the output predictions to a vector */
-
     Matrix temp  = matrix_new_(SAMPLES, 1); /* temporary matrix to be converted */
-    Vector preds = vector_new_(SAMPLES); /* vector given to MSE */
+    Vector preds = vector_new_(SAMPLES);    /* vector given to MSE */
 
     forward(nn, X, temp, preds);
 
@@ -128,8 +122,8 @@ void neuralnet_run_eg(int iters) {
     /*** training loop ***/
 
     float loss;
-    float lr = 0.05F;   /* learning rate */
-    float eps = 1.0E-6F; /* constant term in symmetric derivative approx */
+    float lr = 0.05F; /* learning rate */
+    float eps = 1.0E-6F; /* h */
 
     for (int iter = 0; iter < iters; iter++) {
 
@@ -144,13 +138,19 @@ void neuralnet_run_eg(int iters) {
         Matrix dws[nn->nlayers];
         Vector dbs[nn->nlayers];
 
+        /* calculate dW and db for each layer of the net */ 
         compute_grads_(nn, X, temp, preds, y, eps, dws, dbs);
+
+        /* update params */ 
         nn_update(nn, dws, dbs, lr);
+
+        /* TODO: this can be removed & replaced by a one-time alloc */ 
         for (int i = 0; i < nn->nlayers; i++) { 
             matrix_free(dws[i]); vector_free(dbs[i]); 
         }
     }
 
+    /*** log results ***/ 
     printf("\ninitial loss: %.4f\n", init_loss);
     printf(  "final loss:   %.4f\n\n", loss);
 
@@ -162,6 +162,7 @@ void neuralnet_run_eg(int iters) {
     for (int i = 0; i < 15; i++) { printf("%.2f ", preds->d[i]); }
     puts("");
     
+    /* free allocated resources */
     matrix_free(X); matrix_free(temp);
     vector_free(y); vector_free(preds);
     nn_free(nn);
